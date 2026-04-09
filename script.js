d26e4d5fb50e7513ad523f55c35732e363bc0169
shamimakhurshid76 <shamimakhurshid76@gmail.com>
2026-04-09 22:34:28 +0530

    Added Urban Wedding website files

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function submitForm() {
+  alert("Thank you! We will contact you soon.");
+}
